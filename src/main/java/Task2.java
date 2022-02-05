8690661c59af148bdf81aaee876415ac96efd7fe
Iryna Vovnytska <mimotina2015@gmail.com>
2022-02-05 19:00:18 +0200

    HW4 TASK2

---
+++ src/main/java/Task2.java
@@ -0,0 +1,11 @@
+public class Task2 {
+    public static void main(String[] args) {
+
+        float earn = 5000;
+        float par = 5000;
+        float euro = 28;
+
+        System.out.println("sum in euro " + (earn + par)/euro  );
+    }
+
+}

509252b76ba10000be2cea8f1cd23b6b93bfb384
Iryna Vovnytska <mimotina2015@gmail.com>
2022-02-16 20:59:35 +0200

    HW7 3 fix

--- src/main/java/Test3.java
+++ src/main/java/Test3.java
@@ -1,18 +1,25 @@
+import java.util.Scanner;
+
 public class Test3 {
     public static void main(String[] args) {
-        StringBuilder Name = new StringBuilder("Petrov");
-        Name.setLength(15);
-        StringBuilder value = new StringBuilder("5");
-        value.setLength(3);
-        StringBuilder subject = new StringBuilder("Math");
-        subject.setLength(10);
-        Print(Name.toString(), value.toString(), subject.toString());
+        Scanner in = new Scanner(System.in);
+        String Name = in.nextLine();
+        String value = in.nextLine();
+        String subject = in.nextLine();
+         System.out.printf("Студент %-15s получил %-3s по %-10s", Name, value, subject);
+       }
+  }
 
-    }
-    public static void Print(String Name, String value, String subject) {
+
+
+
+
+// StringBuilder Name = new StringBuilder("Petrov");
+// Name.setLength(15);
+// StringBuilder value = new StringBuilder("5");
+// value.setLength(3);
+//  StringBuilder subject = new StringBuilder("Math");
+//  subject.setLength(10);
+ /*public static void Print(String Name, String value, String subject) {
         String pr = String.format("Студент %s получил %s по %s", Name, value, subject);
-        System.out.println(pr);
-    }
-
-}
-
+        System.out.printf(pr)*/

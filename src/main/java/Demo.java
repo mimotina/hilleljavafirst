159d3ca5cc66c76dd52da38442e9490a6fbe042c
Iryna Vovnytska <mimotina2015@gmail.com>
2022-04-15 00:51:36 +0300

    HW14 + fix3

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -10,33 +10,43 @@ public class Demo {
     }
 
     public static void getException() {
-        Scanner scanner = new Scanner(System.in);
 
         try {
-            System.out.println("enter login");
-            String login = scanner.nextLine();
-            if (login.length() < 5 || !(login.contains("_"))) {
-                throw new WrongLoginException("error", "Wrong login");
-            }
-
-            System.out.println("enter password");
-            String password = scanner.nextLine();
-            if (password.length() < 5 || !((password.contains("_")) && (password.contains("!")))) {
-                throw new WrongPasswordException("error", "Password less then 20 symbols or no '_' or no '!'");
-            }
-            System.out.println("enter confirmPassword");
-            String confirmPassword = scanner.nextLine();
-            if (!password.equals(confirmPassword)){
-                throw new WrongPasswordException("error","password not  match");
-            }
-
-
-        System.out.println(password + " everything OK ");
+            login();
+            password();
+            confirmPassword();
         } catch (WrongLoginException s) {
             System.out.println(s.getMessage());
         } catch (WrongPasswordException e) {
             System.out.print(e.getMessage());
-
         }
     }
-}
+
+    public static void login() throws WrongLoginException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter login");
+        String login = scanner.nextLine();
+        if (login.length() < 5 || !(login.contains("_"))) {
+            throw new WrongLoginException("error", "Wrong login");
+        }
+    }
+
+    public static String password() throws WrongPasswordException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter password");
+        String password = scanner.nextLine();
+        if (password.length() < 5 || !((password.contains("_")) && (password.contains("!")))) {
+            throw new WrongPasswordException("error", "Password less then 20 symbols or no '_' or n");
+        }
+
+        return password;
+    }
+
+    public static void confirmPassword() throws WrongPasswordException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter confirmPassword");
+        String confirmPassword = scanner.nextLine();
+
+        if (confirmPassword.equals(password())){
+        throw new WrongPasswordException("error", "password not  match");
+    }}}

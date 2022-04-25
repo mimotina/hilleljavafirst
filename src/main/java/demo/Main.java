f14bb76968a5b9167aa7e60df38d429885fbce0b
Iryna Vovnytska <mimotina2015@gmail.com>
2022-04-25 17:54:46 +0300

    HW17+fix

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,39 +1,15 @@
 package demo;
-import static demo.Week.*;
 
-
-import educational_center.abstaction.Subjects;
-import educational_center.members.Student;
-import educational_center.members.Teacher;
-
-import java.util.HashSet;
-import java.util.Set;
+import static demo.ProductMarket.*;
 
 public class Main {
-    public static void main(String[] args){
-        Teacher course = new Teacher(10);
-        Week[] weeks = {MONDAY,TUESDAY, WEDNESDAY, THURSDAY, FRIDAY};
-        Set<Subjects> subject = new HashSet<>();
-        subject.add(new Subjects("Java"));
-        subject.add(new Subjects("JS"));
-        subject.add(new Subjects("HTML"));
-
-        for (Subjects s:subject){
-            System.out.println(s);
-        }
-
-       Student student = new Student("Ira", "ira@google.com", subject, weeks,2);
-       Student student1 = new Student("Katya", "katya@google.com", subject, weeks,2);
-
-
-        course.addStudent(student);
-       course.addStudent(student1);
-
-        course.work();
-
-        System.out.println(MONDAY.getName()+ " " + MONDAY.getDayNumber());
-        System.out.println(student);
-
+    public static void main(String[] args) {
+        productName();
+        productAbc();
+        priceHigh();
+        priceLow();
+        pricesAll();
 
     }
+
 }
--- src/main/java/demo/ProductMarket.java
+++ src/main/java/demo/ProductMarket.java
@@ -3,41 +3,56 @@ package demo;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ProductMarket {
-    public static void main(String[] args) {
-        Product product1 = new Product("water", 10);
-        Product product2 = new Product("sugar", 15);
-        Product product3 = new Product("butter", 8);
-        Product product4 = new Product("milk", 12);
-        Product product5 = new Product("bread", 5);
-
-        List<Product> products= List.of(product1,  product2, product3, product4, product5);
 
 
-        List<String> productName = products.stream()
-                .map(productData -> productData.getName())
-                .collect(Collectors.toList());
-        System.out.println("productName " + productName);
+public  class ProductMarket {
+    static Product product1 = new Product("water", 10);
+    static Product product2 = new Product("sugar", 15);
+    static Product product3 = new Product("butter", 8);
+    static Product product4 = new Product("milk", 12);
+    static Product product5 = new Product("bread", 5);
 
+    static List<Product> products= List.of(product1,  product2, product3, product4, product5);
+
+public static void productName() {
+    List<String> productName = products.stream()
+            .map(Product::getName).sorted()
+            .collect(Collectors.toList());
+     System.out.println("productName " + productName);
+}
+
+
+   public static void productAbc() {
         List<String> productAbc = products.stream()
-                .map(productData -> productData.getName()).sorted()
+                .map(productData -> productData.getName().stripLeading())
                 .collect(Collectors.toList());
-        System.out.println("products by abc " + productAbc);
+        System.out.println("products by abc " + productAbc); }
 
+    public static void priceHigh() {
         List<Product> priceHigh = products.stream()
-                .filter(productData -> productData.getPrice() > 10 )
+                .filter(productData -> productData.getPrice() > 10)
                 .collect(Collectors.toList());
-        System.out.println("priceHigh " + priceHigh);
+        System.out.println("priceHigh " + priceHigh); }
 
+    public static void priceLow(){
         List<Product> priceLow = products.stream()
                 .filter(productData -> productData.getPrice() < 0 )
                 .collect(Collectors.toList());
-        System.out.println("priceLow " + priceLow);
+        System.out.println("priceLow " + priceLow);}
 
+    public static void pricesAll(){
         List<String> pricesAll = products.stream()
                 .map(priceAll -> Integer.toString(priceAll.getPrice()))
                 .collect(Collectors.toList());
-        System.out.println("pricesAll " + pricesAll);
+        System.out.println("pricesAll " + pricesAll);}
 
-    }
+   /* public static void main(String[] args) {
+        productName();
+        productAbc();
+        priceHigh();
+        priceLow();
+        pricesAll();
+        products.forEach(productData -> System.out.println(productData));
+    }*/
+
 }

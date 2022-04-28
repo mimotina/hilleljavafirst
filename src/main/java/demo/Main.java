483699e2d0807185bf0900936f8d11f248296ae7
Iryna Vovnytska <mimotina2015@gmail.com>
2022-04-28 19:26:57 +0300

    HW20

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,15 +1,16 @@
 package demo;
+import java.time.LocalDate;
 import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        Product product1 = new Product("water", 10);
-        Product product2 = new Product("sugar", 15);
-        Product product3 = new Product("butter", 8);
-        Product product4 = new Product("milk", 12);
-        Product product5 = new Product("bread", 5);
+        Product product1 = new Product("water", 10, LocalDate.of(2022,4,25));
+        Product product2 = new Product("sugar", 15, LocalDate.of(2022,4,25));
+        Product product3 = new Product("butter", 8, LocalDate.of(2022,4,25));
+        Product product4 = new Product("milk", 12, LocalDate.of(2022,4,25));
+        Product product5 = new Product("bread", 5,LocalDate.of(2022,4,25));
 
         ProductMarket productMarket = new ProductMarket(List.of(product1, product2, product3, product4, product5));
         System.out.println(productMarket.productAbc());
--- src/main/java/demo/Product.java
+++ src/main/java/demo/Product.java
@@ -1,50 +1,21 @@
 package demo;
 
-import java.util.Objects;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Product {
     private String name;
     private int price;
+    private LocalDate dueDate;
 
-    public Product(String name, int price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-        return price == product.price && Objects.equals(name, product.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, price);
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "name='" + name + '\'' +
-                ", price=" + price +
-                '}';
+    public boolean isFresh() {
+        return dueDate.isAfter(LocalDate.now());
     }
 }
